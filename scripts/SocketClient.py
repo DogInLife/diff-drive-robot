5b5cb06343c80baadd3b831079cdc8f0c8ffe998
oytyeur <khkhlov.ai@mail.ru>
2022-07-11 14:30:53 +0300

    Socket settings upd

--- scripts/SocketClient.py
+++ scripts/SocketClient.py
@@ -1,21 +1,23 @@
 import socket
 
-import serial
+# import serial
 import os
 import sys, select, termios, tty
-from PIL import Image
+# from PIL import Image
 
+'''
 ros_path = '/opt/ros/kinetic/lib/python2.7/dist-packages'
 if ros_path in sys.path:
     sys.path.remove(ros_path)
 import cv2
 sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages')
+'''
 
-HOST = '192.168.0.110'  # IP address
+HOST = '192.168.0.114'  # IP address
 # HOST = '127.0.0.1'  
-PORT = 15000
+PORT = 1500
 
-vel = 60
+vel = 15
 
 def getKey():
     tty.setraw(sys.stdin.fileno())
@@ -29,11 +31,12 @@ def getKey():
  
 
 if __name__=="__main__":
-    print("current speed: ", vel)
+    print("Current speed: ", vel)
     settings = termios.tcgetattr(sys.stdin)
 
     sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sck.connect((HOST, PORT))
+    print('Connected to host', HOST)
     try:
         while True:
             key = getKey()
@@ -45,18 +48,16 @@ if __name__=="__main__":
                 break
             if(key == 'e'):
                 vel += 5
-                print("current speed: ", vel)
+                print("Current speed: ", vel)
             if(key == 'q'):
                 vel -= 5
-                print("current speed: ", vel)
+                print("Current speed: ", vel)
 
 
     except Exception as e:
         print(e)
     finally:
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)                 
-
-
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
 
 
 
--- scripts/SocketServer.py
+++ scripts/SocketServer.py
@@ -2,12 +2,13 @@ import socket, select
 import serial
 import sys
 
-
+'''
 ros_path = '/opt/ros/kinetic/lib/python2.7/dist-packages'
 if ros_path in sys.path:
     sys.path.remove(ros_path)
 import cv2
 sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages')
+'''
 
 s = serial.Serial()
 s.baudrate = 9600
@@ -22,17 +23,17 @@ s.open()
 # AF_INET - IPv4
 # ========================
 
-HOST = '192.168.0.110'
+HOST = '192.168.0.114'
 # HOST = '127.0.0.1'
-PORT = 15000
+PORT = 1500
 
 
 sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sck.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sck.bind((HOST, PORT))
-sck.listen()
+sck.listen(1)
 conn, addr = sck.accept()
-print("connect to: ", addr)
+print("Connected to: ", addr)
 
 
 while True:
