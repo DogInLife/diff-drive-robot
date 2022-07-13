510be78badb1482f28e311b0da4e28450c3aba76
oytyeur <khkhlov.ai@mail.ru>
2022-07-13 20:41:38 +0300

    upd

--- scripts/server.py
+++ scripts/server.py
@@ -43,7 +43,6 @@ while True:
         if (data == b'\x03'):
             ser.write(b's')
             break
-
         ser.write(data) # send to serial (arduino)
 
     ser_recv = ser.read(ser.in_waiting).decode('ascii') # what is received from serial
