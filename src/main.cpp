b4355dcade67364c88d1aecbaa4ba5e56f2d1704
oytyeur <khkhlov.ai@mail.ru>
2022-07-15 21:45:10 +0300

    added wheel angle PID control

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 #include "twoWheeledRobot.h"
 #include "constants.h"
 
-byte dt = 50;
-int whl_vel = 30;
+byte del = 50; // задержка
+int whl_vel = 30; // скорость колеса [об/мин]
 
 void setup() {
   
@@ -12,11 +12,11 @@ void setup() {
   robot.setEncoderPins(ENCODER_PIN_L, ENCODER_PIN_R);
   robot.setDriverPins(DRIVER_PWM_PIN_A, DRIVER_IN_A2, DRIVER_IN_A1 , DRIVER_IN_B1, DRIVER_IN_B2,  DRIVER_PWM_PIN_B);
   // robot.tunePID(5.3, 4.8, 0);
-  robot.tudePID(0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+  robot.tunePID(0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
 
   // robot.serialControl();
   // robot.manualControl(dt);
-  robot.rot_test(whl_vel, dt);
+  robot.rot_test(whl_vel, del);
 
   // float xGoal = 1;
   // float yGoal = 1;
