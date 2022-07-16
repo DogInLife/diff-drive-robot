2818ddd25f9c113c6f8d9ca243bca26799fdb2f4
oytyeur <khkhlov.ai@mail.ru>
2022-07-16 19:11:42 +0300

    test

--- lib/TwoWheeledRobot/twoWheeledRobot.h
+++ lib/TwoWheeledRobot/twoWheeledRobot.h
@@ -45,7 +45,7 @@ public:
     void goToGoal(float x_d, float y_d, float dt);
     void manualControl(float dt);
 
-    void rot_test(int whl_vel_des, byte del); // ####################################
+    void rot_test(int whl_vel_des, byte del, bool deb); // ####################################
 
     void goForward(int velL, int velR);
     void turnLeft(int velL, int velR);
