5a55210737b5a01c8709aec06af370a22ad21e5a
oytyeur <khkhlov.ai@mail.ru>
2022-07-16 20:03:40 +0300

    Wheels angles PID deployed

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,14 @@
 
 byte del = 50; // задержка
 int whl_vel_des = 60; // скорость колеса [об/мин]
-bool deb = true; // флаг типа дебаггинга
+bool deb = false; // флаг типа дебаггинга
+
+float KpL;
+float KiL;
+float KdL;
+float KpR;
+float KiR;
+float KdR;
 
 void setup() {
   
@@ -14,8 +21,19 @@ void setup() {
   robot.setDriverPins(DRIVER_PWM_PIN_A, DRIVER_IN_A2, DRIVER_IN_A1 , DRIVER_IN_B1, DRIVER_IN_B2,  DRIVER_PWM_PIN_B);
   // robot.tunePID(5.3, 4.8, 0);
 
-  // ========== П Р О В Е Р Ь   Ф Л А Г   D E B   !!! =============
-  robot.tunePID(20.0, 0.0, 0.1, /*_*/ 25.0, 0.0, 0.1); // (pL, iL, dL, pR, iR, dR)
+  KpL = 20.0;
+  KiL = 0.0;
+  KdL = 0.05;
+// ================  П Р О В Е Р Ь  ============
+// ================     Ф Л А Г     ============
+// ================      D E B      ============
+  KpR = 25.0;
+  KiR = 0.0;
+  KdR = 0.05;
+  
+  robot.tunePID(KpL, KiL, KdL, KpR, KiR, KdR)
+
+  //robot.tunePID(20.0, 0.0, 0.05, /*_*/ 25.0, 0.0, 0.05); // (pL, iL, dL, pR, iR, dR)
 
   // robot.serialControl();
   // robot.manualControl(dt);
