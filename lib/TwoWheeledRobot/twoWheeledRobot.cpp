81caddf208a38560a63292c2d268aa1f1bc6e7ac
oytyeur <khkhlov.ai@mail.ru>
2022-07-16 21:26:10 +0300

    upd

--- lib/TwoWheeledRobot/twoWheeledRobot.cpp
+++ lib/TwoWheeledRobot/twoWheeledRobot.cpp
@@ -337,7 +337,7 @@ void TwoWheeledRobot::rot_test(int whl_vel_des, byte del, bool deb)
         goForward(whl_velL, whl_velR);
       }
 
-      if(q_des >= 3.99)
+      if(q_des >= 3.975)
       {
         Serial.println("Stopping");
         stopMoving();
