6c39154b85ead12b850d12b2fc140c0c5b2730fb
oytyeur <khkhlov.ai@mail.ru>
2022-07-16 22:48:48 +0300

    upd

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 byte del = 50; // задержка
 int whl_vel_des = 60; // скорость колеса [об/мин]
-bool deb = false; // флаг типа дебаггинга
+bool deb = true; // флаг типа дебаггинга
 
 float KpL = 0.0;
 float KiL = 0.0;
@@ -19,7 +19,7 @@ void setup() {
   robot.createWheels(WHEEL_RADIUS, BASE_LENGTH, MAX_VELOCITY);
   robot.setEncoderPins(ENCODER_PIN_L, ENCODER_PIN_R);
   robot.setDriverPins(DRIVER_PWM_PIN_A, DRIVER_IN_A2, DRIVER_IN_A1 , DRIVER_IN_B1, DRIVER_IN_B2,  DRIVER_PWM_PIN_B);
-  // robot.tunePID(5.3, 4.8, 0);
+  robot.tunePID(5.3, 4.8, 0);
 
   KpL = 27.5;
   KiL = 1000.0;
@@ -34,9 +34,9 @@ void setup() {
   robot.tuneWhlPID(KpL, KiL, KdL, KpR, KiR, KdR);
   //robot.tunePID(20.0, 0.0, 0.05, /*_*/ 25.0, 0.0, 0.05); // (pL, iL, dL, pR, iR, dR)
 
-  // robot.serialControl();
+  robot.serialControl();
   //robot.manualControl(del);
-  robot.rot_test(whl_vel_des, del, deb); // ########################
+  //robot.rot_test(whl_vel_des, del, deb); // ########################
 
   // float xGoal = 1;
   // float yGoal = 1;
