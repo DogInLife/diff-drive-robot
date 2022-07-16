7c9c5c88d100d10b570484c191149ce20d79e4b5
oytyeur <khkhlov.ai@mail.ru>
2022-07-16 19:26:22 +0300

    test

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 byte del = 50; // задержка
 int whl_vel_des = 60; // скорость колеса [об/мин]
-bool deb = false; // флаг типа дебаггинга
+bool deb = true; // флаг типа дебаггинга
 
 void setup() {
   
@@ -13,7 +13,9 @@ void setup() {
   robot.setEncoderPins(ENCODER_PIN_L, ENCODER_PIN_R);
   robot.setDriverPins(DRIVER_PWM_PIN_A, DRIVER_IN_A2, DRIVER_IN_A1 , DRIVER_IN_B1, DRIVER_IN_B2,  DRIVER_PWM_PIN_B);
   // robot.tunePID(5.3, 4.8, 0);
-  robot.tunePID(20.0, 0.0, 0.0, /*_*/ 25.0, 0.0, 0.0); // (pL, iL, dL, pR, iR, dR)
+
+  // ========== П Р О В Е Р Ь   Ф Л А Г   D E B   !!! =============
+  robot.tunePID(20.0, 0.0, 0.2, /*_*/ 25.0, 0.0, 0.2); // (pL, iL, dL, pR, iR, dR)
 
   // robot.serialControl();
   // robot.manualControl(dt);
