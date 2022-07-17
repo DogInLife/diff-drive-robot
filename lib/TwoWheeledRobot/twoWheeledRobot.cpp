3b742868aa04f9ccfbd378bada8e98245f20f66d
oytyeur <khkhlov.ai@mail.ru>
2022-07-17 19:48:00 +0300

    g2g test

--- lib/TwoWheeledRobot/twoWheeledRobot.cpp
+++ lib/TwoWheeledRobot/twoWheeledRobot.cpp
@@ -117,8 +117,6 @@ void TwoWheeledRobot::goToGoal(float xGoal, float yGoal, float dt)
   while(!reachedGoal)
   {
     err = pid->computeAngleError(pos.thetaGoal, pos.theta);
-
-    Serial.println("Theta Err: " + String(err, 3));
     
     vel.ang = pid->computeControl(err, dt/1000);
     vel.lin = vel.computeLinearSpeed();
@@ -130,9 +128,6 @@ void TwoWheeledRobot::goToGoal(float xGoal, float yGoal, float dt)
     //Расчет скоростей для каждого двигателя
     float velR = (2*vel.lin + vel.ang*L)/(2*R);
     float velL = (2*vel.lin - vel.ang*L)/(2*R);
-
-    //String msg_whl_vel = "L Vel: " + String(velL, 3) + " R Vel: " + String(velR, 3);
-    //Serial.println(msg_whl_vel);
 
     motorBlockL->setVelocity(velL, vel.maxWheel, newMinRange);
     motorBlockR->setVelocity(velR, vel.maxWheel, newMinRange);
