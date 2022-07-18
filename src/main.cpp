0e3ae7727f510d0323c05c31ebdfd44e401caeb3
oytyeur <khkhlov.ai@mail.ru>
2022-07-18 23:50:33 +0300

    circle trajectory test

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "constants.h"
 
 byte del = 50; // задержка
-int whl_vel_des = 60; // скорость колеса [об/мин]
-bool deb = true; // флаг типа дебаггинга
+int whl_vel_des = 120; // скорость колеса [об/мин]
+bool deb = false; // флаг типа дебаггинга
 
 float KpL = 0.0;
 float KiL = 0.0;
@@ -22,23 +22,23 @@ void setup() {
   //robot.tunePID(5.3, 4.8, 0);
   //robot.tunePID(20.0, 8.0, 0.0);
 
-  KpL = 250.0;
-  KiL = 5000.0;
+  KpL = 300.0;
+  KiL = 6000.0;
   KdL = 0.5;
 // ================  П Р О В Е Р Ь  ============
 // ================     Ф Л А Г     ============
 // ================      D E B      ============
-  KpR = 250.0;
-  KiR = 5000.0;
+  KpR = 300.0;
+  KiR = 6000.0;
   KdR = 0.5;
   
   robot.tuneWhlPID(KpL, KiL, KdL, KpR, KiR, KdR);
   //robot.tunePID(20.0, 0.0, 0.05, /*_*/ 25.0, 0.0, 0.05); // (pL, iL, dL, pR, iR, dR)
 
-  robot.serialControl();
+  //robot.serialControl();
   //robot.goCircle(1.0, 8);
   //robot.manualControl(del);
-  //robot.rot_test(whl_vel_des, del, deb); // ########################
+  robot.rot_test(whl_vel_des, del, deb); // ########################
 
   // float xGoal = 1;
   // float yGoal = 1;
