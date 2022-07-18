01af47d3ca704678d08b0286ecde00e0fc286ea9
oytyeur <khkhlov.ai@mail.ru>
2022-07-18 21:37:43 +0300

    circle trajectory test

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ void setup() {
   robot.setEncoderPins(ENCODER_PIN_L, ENCODER_PIN_R);
   robot.setDriverPins(DRIVER_PWM_PIN_A, DRIVER_IN_A2, DRIVER_IN_A1 , DRIVER_IN_B1, DRIVER_IN_B2,  DRIVER_PWM_PIN_B);
   //robot.tunePID(5.3, 4.8, 0);
-  robot.tunePID(1.2, 1.4, 0);
+  robot.tunePID(5.2, 1.4, 0.5);
 
   KpL = 27.5;
   KiL = 1000.0;
