91b43e744572591c558cf0c6f104278b4cae42d9
oytyeur <khkhlov.ai@mail.ru>
2022-07-19 23:54:09 +0300

    kinematic model improved

--- lib/Position/position.cpp
+++ lib/Position/position.cpp
@@ -31,6 +31,20 @@ void Position::computeCurentPose(float D_L, float D_R, float D_C, float L)
 // оценка текущей позиции робота по данным с энкодеров
 void Position::estCurrentPosition(float deltaAng_L, float deltaAng_R, float r, float L)
 {
+
+    // float cos_th = 0.0;
+    // float sin_th = 0.0;
+
+    // if (theta == 3.141593/2)
+    //     cos_th = 0.0;
+    // else
+    //     cos_th = cos(theta);
+    
+    // if (theta == 0.0)
+    //     sin_th = 0.0;
+    // else
+    //     sin_th = sin(theta);
+
     float deltaTheta = r * (deltaAng_R - deltaAng_L) / L;
     float curveR = (L * (deltaAng_R + deltaAng_L)) / (2.0 * (deltaAng_R - deltaAng_L));
     
@@ -42,7 +56,7 @@ void Position::estCurrentPosition(float deltaAng_L, float deltaAng_R, float r, f
     float deltaX = dXR * cos(theta) - dYR * sin(theta);
     float deltaY = dXR * sin(theta) + dYR * cos(theta);
 
-    x += deltaX;
-    y += deltaY;
-    theta += deltaTheta;
+    x = x + deltaX;
+    y = y + deltaY;
+    theta = theta + deltaTheta;
 }
