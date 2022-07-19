8c042392503acd502a887e3b0d4d2263ea410ce3
oytyeur <khkhlov.ai@mail.ru>
2022-07-20 02:57:52 +0300

    kinematic model improved

--- lib/Position/position.cpp
+++ lib/Position/position.cpp
@@ -98,5 +98,10 @@ void Position::estCurrentPosition(float deltaAng_L, float deltaAng_R, float r, f
 
     x = x + deltaX;
     y = y + deltaY;
-    theta = theta + deltaTheta;
+    
+    float nextTheta = theta + deltaTheta;
+    if(newTheta > 3.141593) theta = nextTheta - 2*3.141593;
+    else if(newTheta < -3.141593) theta = newTheta + 2*3.141593;
+    else theta = newTheta; 
+
 }
