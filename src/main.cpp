d7aaba0afca779ce0940977c6d7785a1684dc135
oytyeur <khkhlov.ai@mail.ru>
2022-07-20 00:44:49 +0300

    kinematic model improved

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ void setup() {
   robot.setEncoderPins(ENCODER_PIN_L, ENCODER_PIN_R);
   robot.setDriverPins(DRIVER_PWM_PIN_A, DRIVER_IN_A2, DRIVER_IN_A1 , DRIVER_IN_B1, DRIVER_IN_B2,  DRIVER_PWM_PIN_B);
   //robot.tunePID(5.3, 4.8, 0);
-  robot.tunePID(1.0, 0.0, 0.5);
+  robot.tunePID(0.1, 0.0, 0.0);
 
 
 // ====== Д Л Я  120 ОБ/МИН ======
