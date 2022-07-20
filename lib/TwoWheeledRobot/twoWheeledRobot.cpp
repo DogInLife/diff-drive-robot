e9083bef86d9f6c8bfbc0d81eb0d7b731e6ee410
oytyeur <khkhlov.ai@mail.ru>
2022-07-20 08:15:10 +0300

    kinematic model improved

--- lib/TwoWheeledRobot/twoWheeledRobot.cpp
+++ lib/TwoWheeledRobot/twoWheeledRobot.cpp
@@ -204,7 +204,7 @@ void TwoWheeledRobot::goToGoal(float xGoal, float yGoal, bool isFinish, float dt
     Serial.println(msg_pos);
  
 
-    if((abs(xGoal-pos.x) < 0.05) && (abs(yGoal-pos.y) < 0.05))
+    if((abs(xGoal-pos.x) < 0.03) && (abs(yGoal-pos.y) < 0.03))
     {
       Serial.println("PT REACHED");
       Serial.print("err_X: "); Serial.print(pos.x-xGoal, 3);
