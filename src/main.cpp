0224bd740cae81c8a8b18d3a13340c7421a86ae5
oytyeur <khkhlov.ai@mail.ru>
2022-07-22 18:27:04 +0300

    rfid processing

--- src/main.cpp
+++ src/main.cpp
@@ -85,7 +85,6 @@ void setup() {
 
 void loop() {
 
-  //Serial.println("AYAAAA");
 	// Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
 	if ( ! mfrc522.PICC_IsNewCardPresent()) {
 		return;
