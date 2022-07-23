7bc5c2591b8b600d3b601cac82c4c4525d0ab13d
oytyeur <khkhlov.ai@mail.ru>
2022-07-24 00:03:05 +0300

    upd

--- lib/RFIDReader/RFIDReader.cpp
+++ lib/RFIDReader/RFIDReader.cpp
@@ -5,8 +5,13 @@ RFIDReader::RFIDReader(byte SS_PIN, byte RST_PIN) {
     //MFRC522 reader(SS_PIN, RST_PIN);
     //reader = new MFRC522(SS_PIN, RST_PIN);
     reader = new MFRC522(SS_PIN, RST_PIN);
-    //this->reader = mfrc522;
-    this->readerStart();
+
+    while(!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+	SPI.begin();			// Init SPI bus
+	reader->PCD_Init();		// Init MFRC522
+	delay(4);				// Optional delay. Some board do need more time after init to be ready, see Readme
+	reader->PCD_DumpVersionToSerial();	// Show details of PCD - MFRC522 Card Reader details
+    //this->readerStart();
 }
 
 // RFIDReader::~RFIDReader(byte SS_PIN, byte RST_PIN) {
