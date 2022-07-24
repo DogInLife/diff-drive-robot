3e5fb336b42f87aa0e2a191f255a636045d0e416
oytyeur <khkhlov.ai@mail.ru>
2022-07-24 23:18:05 +0300

    upd

--- src/main.cpp
+++ src/main.cpp
@@ -34,13 +34,13 @@ void setup() {
   Serial.begin(9600);
   // Serial.println(F("SETUP"));
   // rfidReader->readerStart();
-  robot->createWheels(WHEEL_RADIUS, BASE_LENGTH, MAX_VELOCITY);
-  robot->setEncoderPins(ENCODER_PIN_L, ENCODER_PIN_R);
-  robot->setDriverPins(DRIVER_PWM_PIN_A, DRIVER_IN_A2, DRIVER_IN_A1 , DRIVER_IN_B1, DRIVER_IN_B2,  DRIVER_PWM_PIN_B);
+  robot.createWheels(WHEEL_RADIUS, BASE_LENGTH, MAX_VELOCITY);
+  robot.setEncoderPins(ENCODER_PIN_L, ENCODER_PIN_R);
+  robot.setDriverPins(DRIVER_PWM_PIN_A, DRIVER_IN_A2, DRIVER_IN_A1 , DRIVER_IN_B1, DRIVER_IN_B2,  DRIVER_PWM_PIN_B);
   //robot.tunePID(5.3, 4.8, 0);
   //robot.tunePID(0.7, 1.5, 0.0);
   //robot.tunePID(4, 1.5, 0); // РАБОТАЕТ НА КРУГЕ (ну так)
-  robot->tunePID(3.5, 1.8, 0);
+  robot.tunePID(3.5, 1.8, 0);
 // ====== Д Л Я  120 ОБ/МИН ======
 //   KpL = 600.0;
 //   KiL = 12000.0;
@@ -62,7 +62,7 @@ void setup() {
   KpR = 250.0;
   KiR = 5000.0;
   KdR = 0.5;
-  robot->tuneWhlPID(KpL, KiL, KdL, KpR, KiR, KdR);
+  robot.tuneWhlPID(KpL, KiL, KdL, KpR, KiR, KdR);
   //robot->serialControl(deb);
   //robot.goCircle(1.0, 8);
   //robot.manualControl(del);
@@ -78,7 +78,7 @@ void setup() {
 void loop() {
   // rfidReader->checkReaderData();
   //delay(50);
-  robot->serialControl(deb);
+  robot.serialControl(deb);
 }
 
 
