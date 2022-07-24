e486fc329aa891c3858f748d39cd6aa420e107f9
oytyeur <khkhlov.ai@mail.ru>
2022-07-24 22:46:51 +0300

    upd

--- lib/RFIDReader/RFIDReader.cpp
+++ lib/RFIDReader/RFIDReader.cpp
@@ -46,14 +46,27 @@ void RFIDReader::checkReaderData() {
 
 void RFIDReader::getUID() {
     //this->reader->PICC_DumpDetailsToSerial(&(this->reader->uid));
-    for(byte i = 0; i < &(this->reader->uid)->size; i++) {
-		if(&(this->reader->uid)->uidByte[i] < 0x10)
+    Uid *thisUid = &(this->reader->uid);
+
+
+    for(byte i = 0; i < thisUid->size; i++) {
+		if(thisUid->uidByte[i] < 0x10)
 			// Serial.print(F(" 0")); // ####
 			Serial.print(F("0")); // ###
 		else
 			//Serial.print(F(" ")); // ###
-		Serial.print(&(this->reader->uid)->uidByte[i], HEX);
-	} 
+		Serial.print(thisUid->uidByte[i], HEX);
+    }
+
+    // for(byte i = 0; i < this->reader->uid->size; i++) {
+	// 	if(this->reader->uid->uidByte[i] < 0x10)
+	// 		// Serial.print(F(" 0")); // ####
+	// 		Serial.print(F("0")); // ###
+	// 	else
+	// 		//Serial.print(F(" ")); // ###
+	// 	Serial.print(this->reader->uid)->uidByte[i], HEX);
+	// }
+
     //Serial.println(this->reader->uid->)
     Serial.println();
     this->reader->PICC_HaltA();
