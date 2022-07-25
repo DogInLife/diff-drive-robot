d054423f25f323ad1018502019732a2e8df29785
oytyeur <khkhlov.ai@mail.ru>
2022-07-25 14:30:26 +0300

    upd

--- lib/Velocity/velocity.cpp
+++ lib/Velocity/velocity.cpp
@@ -7,8 +7,8 @@ Velocity::Velocity()
 
 float Velocity::computeLinearSpeed()
 {
-   if(fabs(ang) < 0.1)
-      return 0.5*maxRobot*100*square(0.1 - fabs(ang));
+   if(fabs(ang) < 0.5)
+      return maxRobot*4*square(0.5 - fabs(ang));
    else 
       return 0; 
 
