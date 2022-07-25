13622eac3f317a2b330a76bd132187e1ced598bc
oytyeur <khkhlov.ai@mail.ru>
2022-07-25 14:07:38 +0300

    upd

--- lib/Velocity/velocity.cpp
+++ lib/Velocity/velocity.cpp
@@ -10,7 +10,7 @@ float Velocity::computeLinearSpeed()
    if(fabs(ang) < 0.01)
       return 0.5*maxRobot*10000*square(0.01 - fabs(ang));
    else 
-      return 0.5*maxRobot; 
+      return 0; 
 
    //return 0.6*maxRobot/(square(fabs(ang)) + 1); // =======  А ЭТУ ШТУКУ МОЖНО ИСПОЛЬЗОВАТЬ КАК-ТО ПРИ УГЛОВАТЫХ ТРАЕКТОРИЯХ
    //return maxRobot*2.0/5.0;
