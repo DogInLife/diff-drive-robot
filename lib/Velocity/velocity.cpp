5f6af7f072e334944f3d6ef313624c6089668ae1
oytyeur <khkhlov.ai@mail.ru>
2022-07-27 22:33:14 +0300

    upd

--- lib/Velocity/velocity.cpp
+++ lib/Velocity/velocity.cpp
@@ -8,13 +8,13 @@ Velocity::Velocity()
 
 float Velocity::computeLinearSpeed(float err)
 {
-   if(fabs(err) < 0.5)
-      return 1.5*maxRobot*(0.5 - fabs(err))/0.5;
-   else 
-      return 0;
+   // if(fabs(err) < 0.25)
+   //    return 1.5*maxRobot*(0.25 - fabs(err))/0.25;
+   // else 
+   //    return 0;
 
    //return 0.6*(0.5*maxRobot*(1 + cos(err)));
-   //return 0.6*maxRobot/(square(fabs(ang)) + 1); // =======  А ЭТУ ШТУКУ МОЖНО ИСПОЛЬЗОВАТЬ КАК-ТО ПРИ УГЛОВАТЫХ ТРАЕКТОРИЯХ
+   return 0.6*maxRobot/(square(fabs(ang)) + 1); // =======  А ЭТУ ШТУКУ МОЖНО ИСПОЛЬЗОВАТЬ КАК-ТО ПРИ УГЛОВАТЫХ ТРАЕКТОРИЯХ
    //return maxRobot*1.5/5.0;
 }
 
