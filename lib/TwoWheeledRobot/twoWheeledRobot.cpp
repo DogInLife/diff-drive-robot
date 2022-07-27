cff6e210f1bcb7d81f3f9a626212fa7a23f90993
oytyeur <khkhlov.ai@mail.ru>
2022-07-27 23:50:53 +0300

    upd

--- lib/TwoWheeledRobot/twoWheeledRobot.cpp
+++ lib/TwoWheeledRobot/twoWheeledRobot.cpp
@@ -109,7 +109,7 @@ void TwoWheeledRobot::serialControl(bool deb) {
 
         case ('g'):
           Serial.println("========= GO GO GO =========");
-          goToGoal(0.6, 0.6, true, 50, deb, false, 3);
+          goToGoal(0.61, 0.61, true, 50, deb, false, 3);
           break;
         
         case ('c'):
