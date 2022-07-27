0e91b1b0615858e2314380343e41f346d6d5c6be
oytyeur <khkhlov.ai@mail.ru>
2022-07-27 22:08:47 +0300

    upd

--- lib/Position/position.cpp
+++ lib/Position/position.cpp
@@ -103,11 +103,11 @@ void Position::estCurrentPosition(float deltaAng_L, float deltaAng_R, float r, f
     x = x + deltaX;
     y = y + deltaY;
 
-    if(distWheelC-distWheelPrev >= dPath) {
-        x = x + dPath*avgXerr;
-        y = y + dPath*avgYerr;
-        distWheelPrev = distWheelC;
-    } 
+    // if(distWheelC-distWheelPrev >= dPath) {
+    //     x = x + dPath*avgXerr;
+    //     y = y + dPath*avgYerr;
+    //     distWheelPrev = distWheelC;
+    // } 
     
     float nextTheta = theta + deltaTheta;
     if(nextTheta > 3.141593) theta = nextTheta - 2*3.141593;
