4ed722d9924db4ebbbb7ecacd94e46daf9fab531
oytyeur <khkhlov.ai@mail.ru>
2022-07-28 06:18:27 +0300

    upd

--- lib/TwoWheeledRobot/twoWheeledRobot.cpp
+++ lib/TwoWheeledRobot/twoWheeledRobot.cpp
@@ -276,10 +276,12 @@ void TwoWheeledRobot::goToGoal(float xGoal, float yGoal, bool isFinish, int del,
     distWheelR = distWheelR + deltaAngR*r;
     distWheelC = (distWheelR + distWheelL) / 2;
 
+    pos.estCurrentPosition(deltaAngL, deltaAngR, r, L);
     String msg_pos = "X: " + String(pos.x, 3) + " Y: " + String(pos.y, 3) + " Th: " + String(pos.theta, 3);
     Serial.println(msg_pos);
-    
-    pos.estCurrentPosition(deltaAngL, deltaAngR, r, L, distWheelC);
+
+    pos.correctPosEst(distWheelC);
+
     if(pos.corrected) {
       Serial.println("CORRECTED X: " + String(pos.x, 3) + " Y: " + String(pos.y, 3));
       pos.corrected = false;
