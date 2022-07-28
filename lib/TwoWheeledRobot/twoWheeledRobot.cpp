9a70cd4e08f0b45601a776b0b545c5a3e9fa674f
oytyeur <khkhlov.ai@mail.ru>
2022-07-28 14:31:48 +0300

    v-omega motion test

--- lib/TwoWheeledRobot/twoWheeledRobot.cpp
+++ lib/TwoWheeledRobot/twoWheeledRobot.cpp
@@ -114,7 +114,7 @@ void TwoWheeledRobot::serialControl(bool deb) {
         
         case ('c'):
           Serial.println("====== Circle trajectory ======");
-          goCircle(0.61, 16, deb);
+          goCircle(0.61, 10, deb);
           break;
 
         case ('t'):
