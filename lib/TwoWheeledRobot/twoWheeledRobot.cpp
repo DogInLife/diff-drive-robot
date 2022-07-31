4b4d3ca057fb6af02890ad9e1f385c7940c995ac
oytyeur <khkhlov.ai@mail.ru>
2022-07-31 13:04:50 +0300

    RFID multiple circles test

--- lib/TwoWheeledRobot/twoWheeledRobot.cpp
+++ lib/TwoWheeledRobot/twoWheeledRobot.cpp
@@ -104,7 +104,7 @@ void TwoWheeledRobot::serialControl(bool deb) {
         
         case ('c'):
           Serial.println("====== Circle trajectory ======");
-          goCircle(0.61, 32, deb, 5);
+          goCircle(0.61, 32, deb, 10);
           break;
 
         case ('t'):
